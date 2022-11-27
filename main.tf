45be380d98c23fcd9ae4c058d279f95d3fd73efa
Sushma <sushma.gayatri451@gmail.com>
2022-11-27 15:35:01 +0530

    Jenkinsfile

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 module "vpc" {
-    source = "./vendor/modules/vpc"
-    VPC_CIDR = var.VPC_CIDR
-}
+    source              = "./vendor/modules/vpc"
+    VPC_CIDR            = var.VPC_CIDR
+    PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
+    PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+    ENV                 = var.ENV
+}   
 
 
 # Always the source attribute in terraform module can not be parameterized
 
 # if it can not be handled or parameterized the source, how are we going to tell, fetch from x brand and when branches are dynamic
+variable "PUBLIC_SUBNET_CIDR" {}
+variable "PRIVATE_SUBNET_CIDR" {}
+variable "ENV" {}
